cd322227fcaf21c3c7389aabec8642b9236144f1
Ashwin <ashwin.kamalakannan@uoit.net>
2016-08-07 02:00:51 -0400

    changed board to 2D array

--- src/main.cpp
+++ src/main.cpp
@@ -7,29 +7,31 @@
 
 using namespace std;
 
-void drawBoard(string[]);
-void resetBoard(string[]);
+void drawBoard(string[][8]);
+void resetBoard(string[][8]);
 void skipLine(int);
 
-void drawBoard(string board[]) {
+void drawBoard(string board[][8]) {
     skipLine(50);
     int vertical[] = {8,7,6,5,4,3,2,1};
-    int j = 0;
-    for (int i=0; i<64; i++) {
-        if (i%8 == 0) {
-            cout << endl << vertical[j++] << " " << board[i];
+    for (int i=0; i<8; i++) {
+        for (int j=0; j<8; j++) {
+            if (j==0)
+                cout << i+1 << "  " << board[i][j];
+            else
+                cout << board[i][j];
         }
-        else {
-            cout << board[i];
-        }
+        cout << endl;
     }
     cout << "" << endl;
-    cout << "   a  b  c  d  e  f  g  h" << endl;
+    cout << "    a  b  c  d  e  f  g  h" << endl;
 }
 
-void resetBoard(string board[]) {
-    for (int i=0; i<64; i++) {
-        board[i] = "[ ]";
+void resetBoard(string board[][8]) {
+    for (int i=0; i<8; i++) {
+        for (int j=0; j<8; j++) {
+            board[i][j] = "[ ]";
+        }
     }
 }
 
@@ -40,9 +42,9 @@ void skipLine(int x) {
 
 int main()
 {
-    string board[64];
+    string board[8][8];
     resetBoard(board);
     drawBoard(board);
-    
+
     return 0;
 }
