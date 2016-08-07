55f3bf631bf6592232b74312998949f723215e79
Ashwin <ashwin.kamalakannan@uoit.net>
2016-08-06 23:29:14 -0400

    added drawBoard function

--- Source Code/main.cpp
+++ Source Code/main.cpp
@@ -2,13 +2,13 @@
 
 using namespace std;
 
-void drawBoard(string[]) {
-    for (int i=0; i<board.length; i++) {
-			if ((i+1)%8 == 0) {
-				System.out.println(board[i] + "  " + (9 - (i+1)/8) );
-			}
-			else
-				cout << board[i];
+void drawBoard(string board[]) {
+    for (int i=0; i<64; i++) {
+        if ((i+1)%8 == 0) {
+            cout << board[i] << "  " << (9 - (i+1)/8)  << endl;
+        }
+        else
+            cout << board[i];
     }
     cout << "" << endl;
     cout << " 1  2  3  4  5  6  7  8" << endl;
@@ -19,7 +19,7 @@ int main()
     string board[64];
     for (int i=0; i<64; i++) {
         board[i] = "[ ]";
-        cout << board[i];
     }
+    drawBoard(board);
     return 0;
 }
