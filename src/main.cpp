aeb45022749c6b2274ab542a0d98eedc9743feff
Ashwin <ashwin.kamalakannan@uoit.net>
2016-08-07 16:25:00 -0400

    added char arrays for pieces

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 void movePiece(string[8][8]);
 void drawBoard(string[8][8]);
-void resetBoard(string[8][8]);
+void resetBoard(string[8][8], char[8][2], char[8][2]);
 void skipLine(int);
 
 /* gets the location of the piece from user
@@ -23,31 +23,31 @@ void movePiece(string board[8][8]) {
     string raw;
     string letters[] = {"a", "b", "c", "d", "e", "f", "g", "h"};
     int x = 0, y, x2, y2;
-
+    
     cout << "Enter the piece you want to move: ";
     cin >> raw;
     string temp = raw.substr(0,1);
     for (int i=0; i<8; i++) {
         if (letters[i] == temp) {
-            x = i + 1;
+            x = i;
             break;
         }
     }
-    y = (atoi(raw.substr(1,2).c_str()));
+    y = 8 - (atoi(raw.substr(1,2).c_str()));
 
     cout << "Enter the location you want to move to: ";
     cin >> raw;
     temp = raw.substr(0,1);
     for (int i=0; i<8; i++) {
         if (letters[i] == temp) {
-            x2 = i + 1;
+            x2 = i;
             break;
         }
     }
-    y2 = (atoi(raw.substr(1,2).c_str()));
+    y2 = 8 - (atoi(raw.substr(1,2).c_str()));
 
-    cout << "start - x: " << letters[x-1] << " y: " << y << endl;
-    cout << "end   - x2: " << letters[x2-1] << " y2: " << y2 << endl;
+    //cout << "start - x: " << x << " y: " << y << endl;
+    //cout << "end   - x2: " << x2 << " y2: " << y2 << endl;
 }
 
 void drawBoard(string board[8][8]) {
@@ -65,10 +65,24 @@ void drawBoard(string board[8][8]) {
     cout << "    a  b  c  d  e  f  g  h" << endl;
 }
 
-void resetBoard(string board[8][8]) {
+/* string piece1[][] and piece2[][]
+*  will be piece arrays eventually instead of string arrrays
+*/
+
+void resetBoard(string board[8][8], char piece1[2][8], char piece2[2][8]) {
     for (int i=0; i<8; i++) {
         for (int j=0; j<8; j++) {
-            board[i][j] = ' ';
+            if (i<2)
+                board[i][j] = piece1[i][j];
+            else if (i>5) {
+                cout << i;
+                if (i==6)
+                    board[i][j] = piece2[i-5][j];
+                else
+                    board[i][j] = piece2[i-7][j];
+            }
+            else
+                board[i][j] = ' ';
         }
     }
 }
@@ -80,7 +94,10 @@ void skipLine(int x) {
 
 int main() {
     string board[8][8];
-    resetBoard(board);
+    char piece1[2][8] = {{'c','k','b','q','k','b','k','c'},{'p','p','p','p','p','p','p','p'}};
+    char piece2[2][8] = {{'C','K','B','Q','K','B','K','C'},{'P','P','P','P','P','P','P','P'}};
+    
+    resetBoard(board, piece1, piece2);
     drawBoard(board);
     movePiece(board);
 
